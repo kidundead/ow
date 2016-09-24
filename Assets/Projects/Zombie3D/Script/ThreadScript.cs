444098ca70688cf894c4d93d749484eaa6aab0af
kf_mhh <1569984818@qq.com>
2016-09-24 16:30:22 +0800

    --kongge

--- Assets/Projects/Zombie3D/Script/ThreadScript.cs
+++ Assets/Projects/Zombie3D/Script/ThreadScript.cs
@@ -6,9 +6,6 @@ using System.Threading;
 public class ThreadScript : MonoBehaviour
 {
 
-
-
-
     protected static Vector3 p = Vector3.zero;
     protected static float lastTime = 0;
     protected static object o = new object();
